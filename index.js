b3f5739cf3f638766172bbc5f7b7faf72b3fe854
viqi zevic <vicky.tanzil@gmail.com>
2018-09-02 19:51:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // add solution here
 
 function theBeatlesPlay(musicians, instruments) {
-  
+  var plays = []
+  for (var i = 0; i < musicians.length; i++) {
+    plays.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return plays
 }
